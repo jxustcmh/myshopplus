0f006b14a166ff6c683b1ad43abf95aaa51e6048
jxustcmh <jxrjcmh@163.com>
2019-09-30 11:40:43 +0800

    基于jdbc的方式存储令牌

--- myshopplus-bussiness/business-oauth2/src/main/java/com/hx/myshop/plus/business/conf/AuthorizationServerConfiguration.java
+++ myshopplus-bussiness/business-oauth2/src/main/java/com/hx/myshop/plus/business/conf/AuthorizationServerConfiguration.java
@@ -1,8 +1,11 @@
 package com.hx.myshop.plus.business.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -10,8 +13,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * 配置认证服务器
@@ -25,17 +33,43 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
-
-
     /**
      * 用于支持密码模式
      */
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    /**
+     *  注入数据源，oauth2会自动的对这些表进行操作
+     * @return
+     */
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        // 配置数据源（注意，我使用的是 HikariCP 连接池），以上注解是指定数据源，否则会有冲突
+        return DataSourceBuilder.create().build();
+    }
+
+
+    /**
+     *  指定token的存储方式
+     * @return
+     */
     @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+//        return new InMemoryTokenStore();
+        return new JdbcTokenStore(dataSource());
+    }
+
+    /**
+     *  clientId和secret的存储地址，oauth2会自动地区查询
+     * @return
+     */
+    @Bean
+    public ClientDetailsService jdbcClientDetailsService() {
+        // 基于 JDBC 实现，需要事先在数据库配置客户端信息
+        return new JdbcClientDetailsService(dataSource());
     }
 
     @Override
@@ -51,10 +85,15 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
         security.checkTokenAccess("isAuthenticated()").allowFormAuthenticationForClients();
     }
 
+    /**
+     *  不再使用内存的方式
+     * @param clients
+     * @throws Exception
+     */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                // 使用内存设置
+        clients.withClientDetails(jdbcClientDetailsService());
+              /*  // 使用内存设置
                 .inMemory()
                 // client_id
                 .withClient("client")
@@ -69,6 +108,6 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
                 // 设置访问令牌的有效期，这里是 1 天
                 .accessTokenValiditySeconds(60 * 60 * 24)
                 // 设置刷新令牌的有效期，这里是 30 天
-                .refreshTokenValiditySeconds(60 * 60 * 24 * 30);
+                .refreshTokenValiditySeconds(60 * 60 * 24 * 30);*/
     }
 }
